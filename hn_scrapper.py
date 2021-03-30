375bea5d7402c24d5b7979d3af7956bfb3c9c4c3
zkid18 <daniel.chepenko@yahoo.com>
2021-03-30 13:06:18 +0300

    screapper

--- hn_scrapper.py
+++ hn_scrapper.py
@@ -1,37 +1,84 @@
-import aiohttp
 import asyncio
 import aioredis
 import async_timeout
 import hashlib
 import re
+import sys
 from bs4 import BeautifulSoup
 import random
+import logging
+
+from store import RedisAsyncStore
+
+from aiohttp import ClientSession, ClientTimeout, TCPConnector
 
 URL = 'https://news.ycombinator.com/'
+REQUEST_TIMEOUT = 60
+MAX_HOST_CONNECTIONS = 10
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15',
+}
 
-async def fetch(session, url, sleep=False):
+async def fetch(url):
     '''
-    resp = await session.request(method="GET", url=url, **kwargs)
+    Fetch URL
+    Args:
+        session (str): Page URL
+
+    Returns:
+        HTML text
     '''
+    logging.debug('Downloading url: {}'.format(url))
+    timeout = ClientTimeout(total=REQUEST_TIMEOUT)
+    connector = TCPConnector(limit_per_host=MAX_HOST_CONNECTIONS, ssl=False)
+    try:
+        async with ClientSession(timeout=timeout, headers=HEADERS, connector=connector) as session:
+            async with session.get(url, ssl=False) as response:
+                context = await response.text()
+                return context
+    except Exception as e:
+        logging.error('Downloading error: {} [{}]'.format(url, e))
+        raise
 
-    async with session.get(url, ssl=False) as response:
-        return await response.text()
+async def parse_main_page(page):
+    '''
+    Parse the hackernews main page
+    
+    discission_links - link to the pages within hn website, 
+    like https://news.ycombinator.com/item?id=26628233
 
-async def parse_page(page):
+    stoty_links - links to the exteranl resources
+    https://hpyproject.org/blog/posts/2021/03/hello-hpy/
+
+    ranks - page rank on the main page at the time of the parsing
+
+    On the crawling stage, save the discussion_link id and the last_rank to the cache storage. 
+    Optioonally store the list of seen ranks. 
+    '''
     soup = BeautifulSoup(page, 'html.parser')
     table = soup.find("table", {"class":"itemlist"})
-    comment_links = table.find_all('a', {'href': re.compile(r"^item")})
+    discussion_links = table.find_all('a', {'href': re.compile(r"^item")})
     story_links = table.find_all('a', {"class":"storylink"})
     ranks = table.find_all('span', {"class":"rank"})
-    posts = zip(comment_links, story_links)
+
+    posts = zip(discussion_links, story_links)
     # check if story link already in cache and change it rank
     hashed_links = ["link_id_" + hashlib.md5(links['href'].encode('utf-8')).hexdigest() for links in story_links]
-    return [links['href'] for links in comment_links]
+    return [links['href'] for links in discussion_links]
 
-async def parse_comment_page(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    table = soup.find("table", {"class":"comment-tree"})
-    return len(table.find_all('tr'))
+
+async def parse_article_page(article_url):
+    logging.info('Parsing the article page: {} searching for the links in the comments'.format(article_url))
+    article_page = await fetch(article_url)
+    soup = BeautifulSoup(article_page, 'html.parser')
+    links = soup.find_all(rel="nofollow")
+    valid_links = [link['href'] for link in links]
+    return valid_links
+
+
+async def write_articles_to_disk():
+    return
+
 
 async def write_links_to_store(links_hash, rank):
     pairs = zip(links_hash, rank)
@@ -39,23 +86,78 @@ async def write_links_to_store(links_hash, rank):
     # redis.close()
     # await redis.wait_closed()
    
-async def main():
-    tasks = []
-    async with aiohttp.ClientSession() as session:
-        loop = asyncio.get_event_loop()
-        home_page_html = await fetch(session, URL)
-        comment_links = await parse_page(home_page_html)
-        print(comment_links)
-        for comment_url in comment_links:
-            task = loop.create_task(fetch(session, URL+comment_url, True))
-            tasks.append(task)
-            await asyncio.sleep(1)
-        repsonses = await asyncio.gather(*tasks)
-        print(repsonses)
-        # commetns = await asyncio.gather(*repsonses)
-        # print(commetns)
 
+async def main(output_dir):
+    tasks = []
+
+    home_page_html = await fetch(URL)
+    articles = await parse_main_page(home_page_html)
+
+    # TO-DO check if link has been downloaded and processed
+    logging.info('Parsed the main page: {} new articles'.format(len(articles)))
+
+    for article_url in articles:
+        article_dir = output_dir.joinpath(article.id)
+        article_dir.mkdir(parents=True, exist_ok=True)
+        
+        task = asyncio.create_task(parse_article_page(URL+article_url))
+        tasks.append(task)
+        await asyncio.sleep(1)
+    await asyncio.gather(*tasks)
+
+
+async def monitor_main():
+    while True:
+        try:
+            await asyncio.wait_for(parse_main_page, timeout=interval)
+        except Exception as e:
+            logging.error('Crawler failed: {}'.format(e))
+        await asyncio.sleep(interval)
+
+
+def setup_logger(is_debug):
+    ''' Setup logger conf
+    Args:
+        is_debug(bool): Debug flag
+    '''
+    logging.basicConfig(level=logging.DEBUG if is_debug else logging.INFO,
+                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+
+
+def parse_arguments():
+    """
+    Get program arguments
+    Returns:
+        argparse.Namespace: Program arguments
+    """
+    parser = argparse.ArgumentParser(
+        description='Async crawler for news.ycombinator.com'
+    )
+    parser.add_argument('-o', '--output', type=str, default=DOCUMENT_ROOT,
+                        help='Output files directory')
+    parser.add_argument('-i', '--interval', type=int, default=CHECK_INTERVAL,
+                        help='Main page check interval (seconds)')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Show debug messages')
+
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    # TO-DO
+
+    args = parse_arguments()
+    setup_logger(args.debug)
+
+    output_dir = pathlib.Path(args.output)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    
+    if sys.version_info >= (3,7):
+        asyncio.run(main())
+    
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(main(output_dir))
+    finally:
+        loop.close()
+        asyncio.set_event_loop(None)
--- store.py
+++ store.py
@@ -18,63 +18,46 @@ def try_connection(attemps):
     return decorator
 
 
-class RedisSrever:
-    async def __init__(self):
-        self.redis = await aioredis.create_redis('redis://localhost')   
-
-
-
-class RedisServerProtocol(asyncio.Protocol):
-    def __init__(self, db):
-        self._db = db
-
-    def connection_made(self, transport):
-        self.transport = transport
-    
-    def data_received(self, data):
-        # Parse data in [b"SET", b"foo", b"bar"]
-        parsed = data
-        command = parsed[0].lower()
-        key = parsed[1]
-        value = parsed[2]
-        if command == 'get':
-            response = self.db.get(key)
-        elif command == 'set':
-            response = self.db.set(key, value)
-        write_response = response
-        self.transport.write(response)
-
-class RedisStore:
+class RedisAsyncStore:
+    ''' Async Redis connector
+    '''
     def __init__(self, host, port, connect_now = True):
         self.host = host
         self.port = port
         if connect_now:
             self.connect
+    
+    async def connect(self):
+        self.client = await aioredis.create_redis_pool(host=self.host, port=self.port)
+    
+    async def disconnect(self):
+        self.client.close()
+        await self.client.wait_closed()
 
-    def connect(self):
-        self.client = redis.Redis(host=self.host, port=self.port)
-
-    def disconnect(self):
-        self.client.connection_pool.disconnect()
-
-    def ping(self):
+    async def ping(self):
         try:
-            return self.client.ping()
+            return await self.client.ping()
         except redis.ConnectionError:
             raise ConnectionError
 
-    def get(self, key):
+    async def get(self, link_id, key):
         try:
-            value = self.client.get(key)
+            value = await self.client.hget((link_id, key))
             return value.decode('utf-8') if value else value
         except redis.RedisError:
             raise ConnectionError
 
-    def set(self, key, value, seconds):
+    async def set(self, link_id, key, value, seconds):
         try:
-            return self.client.set(key, value, ex=seconds)
+            return await self.client.hmset(link_id, {key:value}, ex=seconds)
         except redis.RedisError:
-            raise ConnectionError    
+            raise ConnectionError  
+
+
+
+'''
+Re-write for async
+'''
 
 class Store:
     MAX_ATTEMPS = 10
@@ -90,32 +73,32 @@ class Store:
         return self.cache_storage.connect()
     
     @try_connection(MAX_ATTEMPS)
-    def cache_get(self, key):
+    def cache_get(self, link_id, key, value):
         '''
         Communication with client-server cache stoarage (i.e. memcache, tarantool, redis)
         '''
-        return self.cache_storage.get(key)
+        return self.cache_storage.get(link_id, key)
     
     @try_connection(MAX_ATTEMPS)
-    def get(self, key):
+    def get(self, link_id, key):
         '''
         Communication with separate key-value stoarage (i.e nosql)
         '''
-        return self.key_value_storage.get(key)
+        return self.key_value_storage.get(link_id, key)
 
     @try_connection(MAX_ATTEMPS)
-    def set(self, key, score, seconds=60*60):
+    def set(self, link_id, key, value, seconds=60*60):
         '''
         Communication with separate key-value stoarage (i.e nosql)
         '''
-        return self.cache_storage.set(key, score, seconds)
+        return self.cache_storage.set(link_id, key, value, seconds)
 
     @try_connection(MAX_ATTEMPS)
-    def cache_set(self, key, score, seconds=60*60):
+    def cache_set(self, link_id, key, value, seconds=60*60):
         '''
         Communication with client-server cache stoarage (i.e. memcache, tarantool, redis)
         '''
         try:
-            return self.storage.set(key, score, seconds)
+            return self.storage.set(link_id, key, value, seconds)
         except:
             raise ConnectionError
